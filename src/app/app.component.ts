01256eb7e4ed681356d0aade2ffba1d1ed400c62
XantX <hp_xantx@hotmail.com>
2021-10-16 18:50:45 -0500

    Develop: added angular material and a base board TODO: implemente service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +16,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend-application';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
