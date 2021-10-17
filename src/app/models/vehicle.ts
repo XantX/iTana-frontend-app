fbbbe759f5d2c441f80a5361aeadfbc892f29fd8
XantX <hp_xantx@hotmail.com>
2021-10-16 21:54:12 -0500

    Develop: added error handling to vehicle services

---
+++ src/app/models/vehicle.ts
@@ -0,0 +1,7 @@
+export interface Vehicle {
+  id: number;
+  year: string;
+  category: string;
+  type: string;
+  number: number;
+}
--- src/app/service/vehicles-api.service.ts
+++ src/app/service/vehicles-api.service.ts
@@ -1,9 +1,24 @@
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {Observable, throwError} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class VehiclesApiService {
   basePath = 'http://localhost:8080/api';
-
   constructor( private http: HttpClient ) { }
+
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+
+  //Api error handling
+  handleError(error: HttpErrorResponse): Observable<never>{
+    if(error.error instanceof ErrorEvent){
+      console.log("An error ocurred: ", error.error.message);
+    }else{
+      console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError('Something happened with requestk plase try again later.');
+  }
+
 }
