58fdf23ff66ad66c3251b28c17562c4b585702fd
XantX <hp_xantx@hotmail.com>
2021-10-17 01:09:19 -0500

    Develop: Added function for delete entity TODO: implemente in table

--- src/app/service/vehicles-api.service.ts
+++ src/app/service/vehicles-api.service.ts
@@ -1,36 +1,51 @@
-import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable, throwError} from 'rxjs';
-import {Vehicle} from '../models/vehicle';
-import {VehicleResource} from '../models/vehicle-resource';
-import { catchError , retry } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { Vehicle } from '../models/vehicle';
+import { VehicleResource } from '../models/vehicle-resource';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VehiclesApiService {
   basePath = 'http://localhost:8080/api/vehicles';
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'})};
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
   //Api error handling
-  handleError(error: HttpErrorResponse): Observable<never>{
-    if(error.error instanceof ErrorEvent){
-      console.log("An error ocurred: ", error.error.message);
-    }else{
-      console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
+  handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      console.log('An error ocurred: ', error.error.message);
+    } else {
+      console.log(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
     }
-    return throwError('Something happened with request please try again later.');
+    return throwError(
+      'Something happened with request please try again later.'
+    );
   }
 
-  getAllVehicles():Observable<Vehicle>{
-    return this.http.get<Vehicle>(this.basePath)
-    .pipe( retry(2), catchError(this.handleError));
+  getAllVehicles(): Observable<Vehicle> {
+    return this.http
+      .get<Vehicle>(this.basePath)
+      .pipe(retry(2), catchError(this.handleError));
   }
-  addVehicles(item: any):Observable<VehicleResource>{
-    return this.http.post<VehicleResource>(this.basePath,this.httpOptions)
-    .pipe(retry(2),catchError(this.handleError));
+  addVehicles(item: any): Observable<VehicleResource> {
+    return this.http
+      .post<VehicleResource>(this.basePath, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
   }
-
+  deleteVehicle(id: number): Observable<any>{
+    return this.http.delete<Vehicle>(`${this.basePath}/id/${id}`,this.httpOptions)
+    .pipe(retry(2), catchError(this.handleError));
+  }
 }
